01cff27537de3475ac0cafd4b1face3c6f7b7755
Ben Williams and Sara Schultz <sara@>
2017-07-06 10:10:24 -0700

    add migration for images table

---
+++ db/migrate/20170706170518_create_images.rb
@@ -0,0 +1,11 @@
+class CreateImages < ActiveRecord::Migration[5.0]
+  def change
+    create_table :images do |t|
+      t.string :name
+      t.string :description
+      t.integer :user_id
+      
+      t.timestamps
+    end
+  end
+end
